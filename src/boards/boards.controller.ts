8a66fce04782a703c6ab3b7fc10c23508d9048d8
filola <hsoorang@naver.com>
2022-06-23 09:38:30 +0000

    [Update] Make DB with postgres

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -9,7 +9,8 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { Board, BoardStatus } from './boards.model';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -17,33 +18,40 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 @Controller('boards')
 export class BoardsController {
   constructor(private boardServicer: BoardsService) {}
-
-  @Get('/')
-  getAllBoard(): Array<Board> {
-    return this.boardServicer.getAllBoards();
-  }
+  // @Get('/')
+  // getAllBoard(): Array<Board> {
+  //   return this.boardServicer.getAllBoards();
+  // }
 
   @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
+  getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardServicer.getBoardById(id);
   }
+  // @Get('/:id')
+  // getBoardById(@Param('id') id: string): Board {
+  //   return this.boardServicer.getBoardById(id);
+  // }
+
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() CreateBoardDto: CreateBoardDto): Board {
+  //   return this.boardServicer.createBoard(CreateBoardDto);
+  // }
 
   @Post()
   @UsePipes(ValidationPipe)
-  createBoard(@Body() CreateBoardDto: CreateBoardDto): Board {
+  createBoard(@Body() CreateBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardServicer.createBoard(CreateBoardDto);
   }
-
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') id: string,
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ) {
-    return this.boardServicer.updateBoardStatus(id, status);
-  }
-
-  @Delete('/:id')
-  deleteBoard(@Param('id') id: string): void {
-    this.boardServicer.deleteBoard(id);
-  }
+  // @Patch('/:id/status')
+  // updateBoardStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  // ) {
+  //   return this.boardServicer.updateBoardStatus(id, status);
+  // }
+  // @Delete('/:id')
+  // deleteBoard(@Param('id') id: string): void {
+  //   this.boardServicer.deleteBoard(id);
+  // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,48 +1,67 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './boards.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = [];
+  constructor(
+    @InjectRepository(BoardRepository) private boardRepository: BoardRepository,
+  ) {}
 
-  getAllBoards(): Array<Board> {
-    return this.boards;
-  }
-
-  getBoardById(Id: string) {
-    const found = this.boards.find((board) => board.id === Id);
+  async getBoardById(id: any): Promise<Board> {
+    const found = await this.boardRepository.findOne(id);
 
     if (!found) {
-      throw new NotFoundException(`Can't find board with id ${Id}`);
+      throw new NotFoundException(`Can't find Board with id ${id}`);
     }
 
     return found;
   }
+  // getAllBoards(): Array<Board> {
+  //   return this.boards;
+  // }
+  // getBoardById(Id: string) {
+  //   const found = this.boards.find((board) => board.id === Id);
+  //   if (!found) {
+  //     throw new NotFoundException(`Can't find board with id ${Id}`);
+  //   }
+  //   return found;
+  // }
 
-  createBoard(CreateBoardDto: CreateBoardDto) {
+  async createBoard(CreateBoardDto: CreateBoardDto) {
     const { title, description } = CreateBoardDto;
-    const board: Board = {
-      id: uuid(),
+
+    const board = this.boardRepository.create({
       title,
       description,
       status: BoardStatus.PUBLIC,
-    };
+    });
 
-    this.boards.push(board);
-
+    await this.boardRepository.save(board);
     return board;
   }
-
-  updateBoardStatus(id: string, status: BoardStatus) {
-    const Board = this.getBoardById(id);
-    Board.status = status;
-    return Board;
-  }
-
-  deleteBoard(id: string) {
-    const found = this.getBoardById(id);
-    this.boards.filter((board) => board.id === found.id);
-  }
+  // createBoard(CreateBoardDto: CreateBoardDto) {
+  //   const { title, description } = CreateBoardDto;
+  //   const board: Board = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: BoardStatus.PUBLIC,
+  //   };
+  //   this.boards.push(board);
+  //   return board;
+  // }
+  // updateBoardStatus(id: string, status: BoardStatus) {
+  //   const Board = this.getBoardById(id);
+  //   Board.status = status;
+  //   return Board;
+  // }
+  // deleteBoard(id: string) {
+  //   const found = this.getBoardById(id);
+  //   this.boards.filter((board) => board.id === found.id);
+  // }
 }
